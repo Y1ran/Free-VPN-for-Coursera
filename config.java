cdf2be6d6562e64d804f6df96f4739a2d549906d
hathackerwang <deanyuton@gmail.com>
2019-02-25 21:52:59 +0800

    Create config.java

---
+++ config.java
@@ -0,0 +1,18 @@
+package com.imooc2;
+import static java.lang.System.out;
+
+public class Tel extends Phone {
+
+	@Override
+	public void call() {
+		// TODO Auto-generated method stub
+		out.println("123");
+	}
+
+	@Override
+	public void message() {
+		// TODO Auto-generated method stub
+		out.println("456");
+	}
+
+}
